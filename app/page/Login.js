a70e8d132db53a40318f81a01423d88ec3ab83e4
Jason.zhang <zhangguodongcs@126.com>
2018-03-01 00:08:51 +0800

    修改登陆页

--- app/page/Login.js
+++ app/page/Login.js
@@ -6,22 +6,62 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { userStore } from '../data/mobx';
 import {Kohana} from "../components";
-import MaterialsIcon from 'react-native-vector-icons/MaterialIcons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 export default class Login extends Component {
   render() {
     return (
-      <View style={Styles.MainView}>
-       
+      <View style={styles.container}>
+        <View style={[styles.card2, { backgroundColor: '#b792a6',height:200,    bottom:100, }]}>
+          <Text style={styles.title}>登录</Text>
+          <Kohana
+            style={[styles.input, { backgroundColor: '#f9f5ed' }]}
+            label={'手机号'}
+            iconClass={FontAwesomeIcon}
+            iconName={'phone'}
+            iconColor={'#ddd'}
+            iconColor={'#f4d29a'}
+            labelStyle={{ color: '#91627b' }}
+            inputStyle={{ color: '#91627b' }}
+            useNativeDriver
+          />
+          <Kohana
+            style={{ backgroundColor: '#f9f5ed' }}
+            label={'密码'}
+            iconClass={FontAwesomeIcon}
+            iconName={'lock'}
+            iconColor={'#f4d29a'}
+            iconSize={40}
+            labelStyle={{ marginTop: 8, color: '#91627b' }}
+            inputStyle={{ color: '#91627b' }}
+            useNativeDriver
+          />
+          
       </View>
+    </View>
     );
   }
 }
 
-const Styles = StyleSheet.create({
-  MainView: {
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
+    backgroundColor: '#b792a6',
     justifyContent: "center",
-    alignItems: "center"
-  }
+    //alignItems: "center"
+  },
+  card2: {
+    padding: 16,
+  },
+  input: {
+    marginTop: 4,
+  },
+  title: {
+    paddingBottom: 16,
+    textAlign: 'center',
+    color: '#404d5b',
+    fontSize: 20,
+    fontWeight: 'bold',
+    opacity: 0.8,
+  },
 });
