cd59021bea1dc2410dd18b7cc37d1d6f5d412a79
Jason.zhang <zhangguodongcs@126.com>
2018-03-04 23:13:48 +0800

    添加用户信息页面

--- android/app/src/main/java/com/babyplan/MainApplication.java
+++ android/app/src/main/java/com/babyplan/MainApplication.java
@@ -3,6 +3,8 @@ package com.babyplan;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.interactable.Interactable;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new Interactable(),
+            new VectorIconsPackage(),
             new VectorIconsPackage(),
             new VectorIconsPackage(),
             new VectorIconsPackage()
