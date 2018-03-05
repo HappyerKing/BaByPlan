da9fe57acec8f1fe5800ce697f59a00c52883b0c
Jason.zhang <zhangguodongcs@126.com>
2018-03-05 18:00:41 +0800

    add datamanager

---
+++ app/common/index.js
@@ -0,0 +1,4 @@
+import DateManager from "./DateManager";
+import theme from "./theme";
+
+export { DateManager, theme };
--- app/page/BabyDiary/DiaryHome.js
+++ app/page/BabyDiary/DiaryHome.js
@@ -2,16 +2,43 @@
  *  日记首页
  */
 
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-import {TopBar} from "../../components";
+import React, { PureComponent } from "react";
+import { View, Text ,StyleSheet} from "react-native";
+import { TopBar } from "../../components";
+import { DateManager } from "../../common";
 
-export default class DiaryHome extends Component {
+export default class DiaryHome extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // 选择年份
+      currentYear: DateManager.getYear(),
+      isDetail: false
+    };
+  }
+
+  componentDidMount(){
+    //初始化data
+  }
+
   render() {
     return (
-      <View style={{flex:1}}>
-        <TopBar title ={"宝贝日记"} />
+      <View style={styles.container}>
+        {/* {this.nav()}
+        {this.top()}
+        {this.table()}
+        {this.bottom()}
+        {this.hud()} */}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(244,244,244,1)',
+    justifyContent: 'space-between',
+    paddingBottom: 30,
+  },
+});
